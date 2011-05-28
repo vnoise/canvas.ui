3d70c21ece68211749d2cee651868b8820e32637
Marco Kuhn <marco@hi-pi.de>
2011-05-28 02:41:32 +0200

    add button

---
+++ button.js
@@ -0,0 +1,28 @@
+var Button = new Class({
+    Extends: Widget,
+
+    initialize: function(options) {
+        this._state = false;
+        this.label = "None";
+
+        Widget.prototype.initialize.call(this, options);
+    },
+
+    drawCanvas: function(context) {
+        if (this._state == false){
+            context.fillStyle = this.bgColor;
+        }else{
+            context.fillStyle = this.frontColor;
+        }
+        context.fillRect(0, 0, this.width(), this.height());
+        context.fillStyle = this.labelColor;
+        context.font = "20px Helvetica";
+        context.fillText(this.label, 2, this.height() - 40, this.width() - 20);
+    },
+
+    onTouchDown: function(event) {
+        this._state = ! this._state; 
+        this.fireEvent("click", this._state);
+        return true;
+    }
+});
